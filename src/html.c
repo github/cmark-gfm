3f2100ca8b73c28eb5a61e296c9fbca545409825
Tracy Lum <11878752+talum@users.noreply.github.com>
2021-09-07 15:47:22 +0000

    Add data-attributes on footnote and footnote refs to allow styling when class is sanitized

--- src/html.c
+++ src/html.c
@@ -66,7 +66,7 @@ static bool S_put_footnote_backref(cmark_html_renderer *renderer, cmark_strbuf *
 
   cmark_strbuf_puts(html, "<a href=\"#fnref:");
   cmark_strbuf_put(html, node->as.literal.data, node->as.literal.len);
-  cmark_strbuf_puts(html, "\" class=\"footnote-backref\" aria-label=\"Jump up\">↩</a>");
+  cmark_strbuf_puts(html, "\" class=\"footnote-backref\" aria-label=\"Jump up\" data-footnote-backref>↩</a>");
 
   if (node->footnote.def_count > 1)
   {
@@ -423,7 +423,7 @@ static int S_render_node(cmark_html_renderer *renderer, cmark_node *node,
 
   case CMARK_NODE_FOOTNOTE_REFERENCE:
     if (entering) {
-      cmark_strbuf_puts(html, "<sup class=\"footnote-ref\"><a href=\"#fn:");
+      cmark_strbuf_puts(html, "<sup class=\"footnote-ref\" data-footnote-ref><a href=\"#fn:");
       cmark_strbuf_put(html, node->parent_footnote_def->as.literal.data, node->parent_footnote_def->as.literal.len);
       cmark_strbuf_puts(html, "\" id=\"fnref:");
       cmark_strbuf_put(html, node->parent_footnote_def->as.literal.data, node->parent_footnote_def->as.literal.len);
