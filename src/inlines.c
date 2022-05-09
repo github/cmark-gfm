af38d1ad7c20ec494436cd7fd81e3a8b086fc809
black-desk <clx814727823@gmail.com>
2022-05-10 00:30:22 +0800

    Remove duplicate code

--- src/inlines.c
+++ src/inlines.c
@@ -1091,10 +1091,7 @@ static cmark_node *handle_close_bracket(cmark_parser *parser, subject *subj) {
   }
 
   if (!opener->active) {
-    // take delimiter off stack
-    pop_bracket(subj);
-    return make_str(subj, subj->pos - 1, subj->pos - 1,
-                    cmark_chunk_literal("]"));
+    goto noMatch;
   }
 
   // If we got here, we matched a potential link/image text.
